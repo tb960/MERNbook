4079794649ea3097f02ed44966191bef7e2a0928
Teo Boon Khang <khangtbk@gmail.com>
2021-01-05 11:17:55 +0800

    added a config file

--- config/connectToDatabase.js
+++ config/connectToDatabase.js
@@ -1,1 +1,18 @@
-//this will need to connect mongoose to database
+const mongoose = require("mongoose");
+const config = require("config");
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(config.get("mongoURI"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("MongoDb is connected...");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectToDatabase;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const connectToDatabase = require('./config/connectToDatabase');
+
+connectToDatabase();
 
 let PORT = process.env.PORT || 5000;
 
