0a97be5a38755cde22477f24594a75c7b2c95fa0
Teo Boon Khang <khangtbk@gmail.com>
2021-01-07 21:18:09 +0800

    add search user by id

--- routes/users.js
+++ routes/users.js
@@ -182,8 +182,9 @@ router.post('/login',
     }
 })
 
-router.put('/search_username', 
-    [check('searchInput', "Search is empty").not().isEmpty()], 
+router.put('/change_user_data', 
+    authentication, 
+    [check("changeUserData", "Input is empty").not().isEmpty()],
     async(req,res) =>{
         try {
             let { userNameFromSearch } = req.body;
@@ -207,6 +208,39 @@ router.put('/search_username',
             res.status(500).send("Server error...");
         }
 
+})
+
+router.put('/search_username', 
+    [check('searchInput', "Search is empty").not().isEmpty()], 
+    async(req,res) =>{
+        try {
+            const { changeUserData } = req.body;
+            const errors = validationResult(req);
+            let user = await User.findById(req.user.id).select("-password");
+        
+            if (!errors.isEmpty())
+              return res.status(400).json({ errors: errors.array() });
+        
+            if (!user) return res.status(404).json("User not found");
+        
+            //userDataToChange -> name,lastName,userName
+        
+            let userDataToChange = req.params.user_data_to_change.toString();
+        
+            if (user[userDataToChange] === changeUserData.toString())
+              return res
+                .status(401)
+                .json("This is the same data that is already in database");
+        
+            user[userDataToChange] = changeUserData.toString();
+        
+            await user.save();
+        
+            res.json("Data is changed");
+          } catch (error) {
+            console.error(error);
+            return res.status(500).json("Server Error...");
+          }
 })
 
 
